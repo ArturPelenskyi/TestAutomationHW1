faf7ab28fcba7bf1b2229a829b15c9025b01736b
ArturPelenskyi <73244422+ArturPelenskyi@users.noreply.github.com>
2020-10-22 11:51:06 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,42 @@
 package com.company;
 
-public class Main {
 
+import java.util.Scanner;
+
+class Main {
     public static void main(String[] args) {
-	// write your code here
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("a=");
+        double a = in.nextDouble();
+        System.out.print("b=");
+        double b = in.nextDouble();
+        System.out.print("c=");
+        double c = in.nextDouble();
+        System.out.print("x=");
+        double x = in.nextDouble();
+        System.out.print("y=");
+        double y = in.nextDouble();
+
+        in.close();
+
+        if((a<=x && b<=y)||(a<=y && b<=x))
+        {
+            System.out.print("Просунути можна!");
+        }
+        else if ((a<=x && c<=y) || (a<=x && c<=y))
+        {
+            System.out.print("Просунути можна!");
+        }
+        else if ((b<=x && c<=y) || (b<=y && c<=x))
+        {
+            System.out.print("Просунути можна!");
+        }
+        else
+        {
+            System.out.print("Просунути неможна! ((");
+        }
+
+
     }
 }
